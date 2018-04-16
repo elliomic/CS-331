3427bb1575e6eaf3e8c4785af888c4d87e40235c
Michael Elliott <elliomic@onid.orst.edu>
2018-04-16 16:24:51 -0700

    Add successor generation function

--- programming1.py
+++ programming1.py
@@ -2,22 +2,110 @@ import sys
 
 class Bank:
 	def __init__(self, chickens, wolves, boat):
-		self.chickens = chickens
-		self.wolves = wolves
-		self.boat = boat
+		self.chickens = num_chickens
+		self.wolves = num_wolves
+		self.boat = has_boat
 
 
 class State:
 	def __init__(self, left, right):
-		self.left = left
-		self.right = right
+		self.left_bank = left
+		self.right_bank = right
+
+	def is_valid(self):
+		if  self.left_bank.num_chickens  >= 0
+		and self.left_bank.num_wolves    >= 0
+		and self.left_bank.num_chickens  >= self.left_bank.num_wolves
+		and self.right_bank.num_chickens >= 0
+		and self.right_bank.num_wolves   >= 0
+		and self.right_bank.num_chickens >= self.right_bank.num_wolves:
+			return True
+		else:
+			return False
 
 
 def read_state_file(filename):
 	state_file = map(lambda l: l.split(","), open(filename, "r").read().split("\n"))
-	return State(
-		Bank(int(state_file[0][0]), int(state_file[0][1]), state_file[0][2] == "1"),
-		Bank(int(state_file[1][0]), int(state_file[1][1]), state_file[1][2] == "1"))
+	return State(left_bank = Bank(num_chickens = int(state_file[0][0]),
+								  num_wolves = int(state_file[0][1]),
+								  has_boat = state_file[0][2] == "1"),
+				 right_bank = Bank(num_chickens = int(state_file[1][0]),
+								   num_wolves = int(state_file[1][1]),
+								   has_boat = state_file[1][2] == "1"))
+
+
+def new_successor(current_state, bank, chickens, wolves):
+	if bank == "left":
+		return State(left_bank =  Bank(current_state.left_bank.num_chickens - chickens,
+									   current_state.left_bank.num_wolves - wolves,
+									   has_boat = False),
+					 right_bank = Bank(current_state.right_bank.num_chickens + chickens,
+									   current_state.right_bank.num_wolves + wolves,
+									   has_boat = True))
+	elif bank == "right":
+		return State(left_bank =  Bank(current_state.left_bank.num_chickens + chickens,
+									   current_state.left_bank.num_wolves + wolves,
+									   has_boat = True),
+					 right_bank = Bank(current_state.right_bank.num_chickens - chickens,
+									   current_state.right_bank.num_wolves - wolves,
+									   has_boat = False))
+
+
+def generate_successors(current_state):
+	successors = list()	
+	if current_state.left_bank.has_boat:
+		# Put one chicken in the boat
+		s = new_successor(current_state, bank = "left", chickens = 1, wolves = 0)
+		if s.is_valid():
+			successors.append(s)
+
+		# Put two chickens in the boat
+		s = new_successor(current_state, bank = "left", chickens = 2, wolves = 0)
+		if s.is_valid():
+			successors.append(s)
+
+		# Put one wolf in the boat
+		s = new_successor(current_state, bank = "left", chickens = 0, wolves = 1)
+		if s.is_valid():
+			successors.append(s)
+
+		# Put one wolf and one chicken in the boat
+		s = new_successor(current_state, bank = "left", chickens = 1, wolves = 1)
+		if s.is_valid():
+			successors.append(s)
+
+		# Put two wolves in the boat
+		s = new_successor(current_state, bank = "left", chickens = 0, wolves = 2)
+		if s.is_valid():
+			successors.append(s)
+
+	elif current_state.right_bank.has_boat:
+		# Put one chicken in the boat
+		s = new_successor(current_state, bank = "right", chickens = 1, wolves = 0)
+		if s.is_valid():
+			successors.append(s)
+
+		# Put two chickens in the boat
+		s = new_successor(current_state, bank = "right", chickens = 2, wolves = 0)
+		if s.is_valid():
+			successors.append(s)
+
+		# Put one wolf in the boat
+		s = new_successor(current_state, bank = "right", chickens = 0, wolves = 1)
+		if s.is_valid():
+			successors.append(s)
+
+		# Put one wolf and one chicken in the boat
+		s = new_successor(current_state, bank = "right", chickens = 1, wolves = 1)
+		if s.is_valid():
+			successors.append(s)
+
+		# Put two wolves in the boat
+		s = new_successor(current_state, bank = "right", chickens = 0, wolves = 2)
+		if s.is_valid():
+			successors.append(s)
+
+	return successors
 
 
 def breadth_first_search(initial_state, goal_state):
